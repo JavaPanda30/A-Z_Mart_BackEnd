8501271edd2f2c6db734670fe43cc6a96e033693
Suyash Pandey <91048475+JavaPanda30@users.noreply.github.com>
2023-11-27 21:42:15 +0000

    Completed Update and Delete Product API

--- backend/controller/productcontroller.js
+++ backend/controller/productcontroller.js
@@ -1,26 +1,80 @@
-const Product = require('../models/productmodel')
+const Product = require("../models/productmodel");
 
-//create product
+//create product --Admin
 exports.createProduct = async (req, res) => {
-    console.log(req.body); 
-    try 
-    {
-        const product = await Product.create(req.body);
-        res.status(201).json({
-            success: true,
-            product
-        });
-    } 
-    catch (error) 
-    {
-        console.error(error);
-        res.status(500).json({
-            success: false,
-            error: 'Internal Server Error'
-        });
+  console.log(req.body);
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
+};
+// Get All Product
+exports.getallproducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    if (products) {
+      return res.status(200).json({
+        success: true,
+        products,
+      });
     }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Error" });
+  }
 };
 
-exports.getallproducts = (req,res)=>{
-    res.status(200).json({"message":"route is working"})
-}
+//update Product --Admin
+
+exports.updateProduct = async (req, res) => {
+  let product = Product.findById(req.param.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Delete Product
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+
+    if (deletedProduct) {
+      return res.status(200).json({
+        success: true,
+        message: "Product Deleted",
+        deletedProduct,
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found. Check the ID.",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Error",
+    });
+  }
+};
--- backend/routes/productroute.js
+++ backend/routes/productroute.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const { getallproducts, createProduct } = require('../controller/productcontroller')
-const router = express.Router()
+const express = require("express");
+const {
+  getallproducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/productcontroller");
+const router = express.Router();
 
-router.route("/products").get(getallproducts);
+router.route("/product").get(getallproducts);
 router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
+router.route("/product/remove/:id").delete(deleteProduct);
 
-
-module.exports = router
+module.exports = router;
