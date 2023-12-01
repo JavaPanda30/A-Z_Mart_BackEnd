8a4e80cb0fe0e5e8cd825d502d60830f9954ef95
Suyash Pandey <91048475+JavaPanda30@users.noreply.github.com>
2023-12-01 19:11:19 +0000

    Added Uncaught Error Handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -2,7 +2,14 @@ const Errorhandler = require("../Utils/Errorhandler");
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.message = err.message || "Internal Server ERROR";
+  err.message = err.message || "Internal Server Error";
+
+  //Wrong mongodb id error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new Errorhandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,30 @@
-const app = require('./app')
-const dotenv = require('dotenv')
-const connectDB = require('./config/database')
+const app = require("./app");
+const dotenv = require("dotenv");
 
+//handling Uncaught Exception
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+const connectDB = require("./config/database");
 //call for process.env
-dotenv.config({path:'backend/config/config.env'})
+dotenv.config({ path: "backend/config/config.env" });
 
 connectDB();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`SERVER IS WORKING ON http://localhost:${process.env.PORT}`)
-})
+const server = app.listen(process.env.PORT, () => {
+  console.log(`SERVER IS WORKING ON http://localhost:${process.env.PORT}`);
+});
+
+//Unhandled Promise Rejection
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
